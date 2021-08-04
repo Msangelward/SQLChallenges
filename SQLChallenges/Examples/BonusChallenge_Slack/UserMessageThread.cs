64ed62128bd2fa3b1e05e3ab643cf327dbeeb19b
Angel Ward <msangelward@gmail.com>
2021-08-03 20:52:30 -0400

    Add project files.

---
+++ SQLChallenges/Examples/BonusChallenge_Slack/UserMessageThread.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLChallenges.Examples.BonusChallenge_Slack
+{
+    public class UserMessageThread
+    {
+        public int UserId { get; set; }
+        public List<User> Users { get; set; }
+        public int MessageThreadId { get; set; }
+        public List<MessageThread> MessageThreads { get; set; }
+    }
+}
---
+++ SQLChallenges/Examples/Challenge2_ATT/Technician.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLChallenges.Examples.Challenge2_ATT
+{
+    public class Technician
+    {
+        public int TechnicianId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal CustomerRating { get; set; }
+        public Vehicle Vehicle { get; set; }
+    }
+}
